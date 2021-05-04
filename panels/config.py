3e5913856644ce0a679212623a03141b700f9b54
Ben Wiswell <benjwiswell@gmail.com>
2021-05-04 12:37:08 -0400

    Rewrite signal and config classes

--- panels/config.py
+++ panels/config.py
@@ -5,26 +5,21 @@ from pygame.event import Event
 
 from .button import Button
 from .panel import Panel
+from ..components.component import Component
 from ..ui.theme import Theme
-
-def dummy_getter() -> object:
-    return 'N/A'
-
-def dummy_setter(obj:object) -> None:
-    pass
 
 def dummy_on_click(event:Event) -> None:
     pass
 
-class Configuration(Panel):
+class Config(Panel):
 
     DEF_SPACING = 5
 
     def __init__(self, 
-                    config_labels:List[str]=['N/A'],
-                    config_options:List[List[object]]=[[]],
-                    config_getters:List[Callable[[], object]]=[dummy_getter],
-                    config_setters:List[Callable[[object], None]]=[dummy_setter],
+                    config_labels:List[str],
+                    config_options:List[List[object]],
+                    config_getters:List[Callable[[], object]],
+                    config_setters:List[Callable[[object], None]],
                 ):
         Panel.__init__(self, config_labels)
         self.config_options = config_options
@@ -46,7 +41,7 @@ class Configuration(Panel):
         curr_ops = self.curr_options()
         n_ops = len(curr_ops)
         if n_ops > 0:
-            x_off = Configuration.DEF_SPACING
+            x_off = Config.DEF_SPACING
             y_off = self.h // 3
             op_w = (self.w - x_off * (n_ops + 1)) // n_ops
             op_h = y_off
@@ -65,17 +60,6 @@ class Configuration(Panel):
     def repos(self, new_pos:Tuple[int, int]) -> None:
         super().repos(new_pos)
         self.create_buttons()
-
-    def add_config(self,
-                    config_label:str,
-                    config_options:List[object]=[],
-                    config_getter:Callable[[], object]=dummy_getter,
-                    config_setter:Callable[[object], None]=dummy_setter
-                ) -> None:
-        self.label_objs.append(config_label)
-        self.config_options.append(config_options)
-        self.config_getters.append(config_getter)
-        self.config_setters.append(config_setter)
 
     def cycle_selection(self) -> None:
         super().cycle_selection()
@@ -110,3 +94,8 @@ class Configuration(Panel):
         if len(self.buttons) > 0:
             for op_btn in self.buttons:
                 op_btn.render(buffer, theme, curr_val == op_btn.curr_label())
+
+
+
+def create_config(component:Component) -> Config:
+    pass
--- signal/signal.py
+++ signal/signal.py
@@ -7,109 +7,159 @@ from bitarray import bitarray
 from bitarray.util import ba2int, int2ba
 
 class Signal:
-    def __init__(self, value:int=0, data_width:int=32, signed:bool=True):
-        self.value = value
-        self.data_width = data_width
-        self.signed = signed
-        self.bits = int2ba(value, data_width, signed=signed)
+    '''
+    A class to represent bit signals and their decimal values.
 
-    @classmethod
-    def from_bits(cls, 
-                    bits:bitarray, 
-                    data_width:int=32, 
-                    signed:bool=True
-                ) -> Signal:
-        value = ba2int(bits, signed)
-        return Signal(value=value, data_width=data_width, signed=signed)
+    Signals act as immutable objects (i.e. the bits, width, signage, and value
+    are not modifiable.)
 
-    @classmethod
-    def left_shift(cls, signal:Signal, target_width:int=32) -> Signal:
-        zeros = '0' * (target_width - signal.data_width)
-        zero_bits = bitarray(zeros)
-        bits = signal.bits + zero_bits
-        return Signal.from_bits(bits, target_width, signal.signed)
-
-    @classmethod
-    def sign_extend(cls, signal:Signal, target_width:int=32) -> Signal:
-        return Signal(signal.value, target_width)
-
-    @classmethod
-    def zero_extend(cls, signal:Signal, target_width:int=32) -> Signal:
-        n_zeros = target_width - signal.data_width
-        if n_zeros > 0:
-            zeros = bitarray(''.join(['0' for _ in range(n_zeros)]))
-            value = ba2int(zeros + signal.bits, signal.signed)
-            return Signal(value, target_width, signal.signed)
+    Attributes:
+        bits (bitarray): the individual bits that make up the signal
+        width (int): the bit-width of the signal
+        signed (bool): the signage of the signal
+        value (int): the integer value of the signal
+    '''
+    def __init__(self, bits:bitarray=None, width:int=32, signed:bool=True):
+        '''
+        Initialize the signal and compute the decimal value.
+        
+        Parameters:
+            bits: the individual bits that make up the signal (default None)
+            width: the bit-width of the signal (default 32)
+            signed: the signage of the signal (default True)
+        '''
+        if bits is None:
+            self._bits = bitarray(width)
+            self._bits.setall(0)
         else:
-            return signal
+            self._bits = bits
+        self._width = width
+        self._signed = signed
+        self._value = ba2int(self._bits, signed=signed)
 
-    def min_val(self) -> int:
-        return int(-pow(2, self.data_width / 2)) if self.signed else 0
+    @classmethod
+    def from_value(cls, value:int, width:int=32, signed:bool=True) -> Signal:
+        '''
+        Create a signal from an integer value instead of a bitarray.
+        
+        Parameters:
+            value: the integer value of the signal
+            width: the bit-width of the signal (default 32)
+            signed: the signage of the signal (default True)
+        '''
+        if signed:
+            if value < 0:
+                min_signal_val = -int(pow(2, width // 2))
+                value = max(min_signal_val, value)
+            else:
+                max_signal_val = int(pow(2, width // 2)) - 1
+                value = min(max_signal_val, value)
+        else:
+            if value < 0:
+                value = 0
+            else:
+                max_signal_val = int(pow(2, width))
+                value = min(max_signal_val, value)
+        bits = int2ba(value, width, signed=signed)
+        return Signal(bits)
 
-    def max_val(self) -> int:
-        power = self.data_width / 2 if self.signed else self.data_width
-        return int(pow(2, power) - 1)
+    @property
+    def bits(self) -> bitarray:
+        '''Get the bits that make up the signal. Setting is disallowed.'''
+        return self._bits
 
-    def get_signed(self) -> bool:
-        return self.signed
+    @property
+    def width(self) -> int:
+        '''Get the bit width of the signal. Setting is disallowed.'''
+        return self._width
+
+    @property
+    def signed(self) -> bool:
+        '''Get the signage of the signal. Setting is disallowed.'''
+        return self._signed
+
+    @property
+    def value(self) -> int:
+        '''Get the decimal value of the signal. Setting is disallowed.'''
+        return self._value
 
     def __str__(self) -> str:
-        type_text = f'{"s" if self.signed else "u"}'
-        type_text += f'{self.data_width}'
-        return f'{self.value} ({type_text})'
+        '''Return a string representation of the signal.'''
+        text = f'{"s" if self._signed else "u"}'
+        text += f'{self._width}'
+        return f'{self.value} ({text})'
 
     def __getitem__(self, key:object) -> bitarray:
-        return self.bits[key]
+        '''Return a bit or slice of bits from the signal.'''
+        return self._bits[key]
 
-    def is_nonzero(self) -> bool:
-        return self.bits.any()
+    def __bool__(self) -> bool:
+        '''Return False if the signal's decimal value is 0 else True.'''
+        return self._bits.any()
 
     def __invert__(self) -> Signal:
-        return Signal.from_bits(~self.bits, self.data_width, self.signed)
+        '''Return the result of inverting each bit of the signal.'''
+        return Signal(~self._bits, self._width, self._signed)
 
     def __or__(self, other:Signal) -> Signal:
-        result = self.bits | other.bits
-        result_width = max(self.data_width, other.data_width)
-        signed = self.signed or other.signed
-        return Signal.from_bits(result, result_width, signed)
+        '''
+        Return the result of a bitwise OR over two signals.
+
+        This operation retains the signage of the signal that self refers to.
+        '''
+        if self._width != other.width:
+            raise ValueError('Signal bit-widths must match!')
+        bits = self._bits | other.bits
+        return Signal(bits, self._width, self._signed)
 
     def __and__(self, other:Signal) -> Signal:
-        result = self.bits & other.bits
-        result_width = max(self.data_width, other.data_width)
-        signed = self.signed or other.signed
-        return Signal.from_bits(result, result_width, signed)
+        '''
+        Return the result of a bitwise AND over two signals.
+
+        This operation retains the signage of the signal that self refers to.
+        '''
+        if self._width != other.width:
+            raise ValueError('Signal bit-widths must match!')
+        bits = self._bits & other.bits
+        return Signal(bits, self._width, self._signed)
+
+    def __add__(self, other:Signal) -> Tuple[Signal, bool]:
+        '''
+        Return the result and overflow flag of adding two signals.
+
+        This operation retains the signage of the signal that self refers to.
+        '''
+        if self._width != other.width:
+            raise ValueError('Signal bit-widths must match!')
+        bits = bitarray(self._width)
+        carry = False
+        for i in range(self._width - 1, -1, -1):
+            bit_res = self[i] + other[i] + carry
+            bits[i] = bit_res % 2
+            carry = bit_res > 1
+        return Signal(bits, self._width, self._signed), carry
 
     def plus(self, other:Signal, carry_in:bool=False) -> Tuple[Signal, bool]:
-        result_width = max(self.data_width, other.data_width)
-        sum_bits = bitarray(result_width)
-        sum_bits.setall(0)
-        carry = carry_in
-        for i in range(result_width - 1, -1, -1):
-            bit_sum = self.bits[i] + other.bits[i] + carry
-            sum_bits[i] = bit_sum % 2
-            carry = bit_sum > 1
-        signed = self.signed or other.signed
-        return Signal.from_bits(sum_bits, result_width, signed), carry
+        
+        '''
+        Return the result and overflow flag of adding two signals.
 
-    def __add__(self, other:Signal) -> Signal:
-        sum_signal, _ = self.plus(other)
-        return sum_signal
+        This version of addition allows for a carry in value. This operation
+        retains the signage of the signal that self refers to.
 
-    def minus(self, other:Signal) -> Tuple[Signal, bool]:
+        Parameters:
+            other: the signal to add to this signal
+            carry_in: the carry in to the add operation (default False)
+        '''
+        carry_sig = Signal.from_value(int(carry_in), self._width, self._signed)
+        carry_sum, carry_overflow = self + carry_sig
+        val_sum, val_overflow = carry_sum + other
+        return val_sum, carry_overflow or val_overflow
+
+    def __sub__(self, other:Signal) -> Tuple[Signal, bool]:
+        '''
+        Return the result and underflow flag of subtracting a signal.
+
+        This operation retains the signage of the signal that self refers to.
+        '''
         return self.plus(~other, True)
-
-    def __sub__(self, other:Signal) -> Signal:
-        diff_signal, _ = self.minus(other)
-        return diff_signal
-
-    def incr_val(self, dir:str) -> None:
-        if dir == '+':
-            new_val = self.value + 1
-            if new_val > self.max_val():
-                new_val = self.min_val()
-        else:
-            new_val = self.value - 1
-            if new_val < self.min_val():
-                new_val = self.max_val()
-        self.value = new_val
-        self.bits = int2ba(new_val, self.data_width, signed=self.signed)
