67efc86c49113d91a5e8568d8daa29ba74089c02
Ben Wiswell <benjwiswell@gmail.com>
2021-08-11 14:59:18 -0400

    feat: update default example processor

--- __main__.py
+++ __main__.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     program = []
     with open('py-virpu/programs/fib.cor', 'r') as file:
         program = [line.strip() for line in file.readlines()]
-    core = Core(program, execute_n=1000, visual=False)
+    core = Core(program, execute_n=0, visual=True)
